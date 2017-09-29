af011b405876f289194ee5831bb91e3ab62028f4
Matthew Hunger <matt.hunger@gmail.com>
2017-09-29 10:52:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  else if int % 5 == 0
+  elseif int % 5 == 0
     "Buzz"
-  else if int % 15 == 0
+  elseif int % 15 == 0
     "Fizzbuzz"
   end
 end
